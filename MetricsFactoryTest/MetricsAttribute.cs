66294cefaad54250bc140e2b0f98839cd8844c78
bowenyang <bowenyang@microsoft.com>
2024-09-25 11:01:12 +0800

    add

---
+++ MetricsFactoryTest/MetricsAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetricsFactoryTest
+{
+    //public class MetricInfoAttribute : Attribute
+    //{
+    //    public string Namespace { get; }
+    //    public string AccountName { get; }
+
+    //    public MetricInfoAttribute(string @namespace, string accountName)
+    //    {
+    //        Namespace = @namespace;
+    //        AccountName = accountName;
+    //    }
+    //}
+}
